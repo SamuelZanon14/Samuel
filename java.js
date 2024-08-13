61b0669b6296a62f5ef86906afff3b1ab925dd05
SamuelZanon14 <samuelzanon14@gmail.com>
2024-08-13 16:04:31 -0300

    c

--- java.js
+++ java.js
@@ -1,3 +1,19 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Botão clicado!');
 });
+
+const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bfbc0'];
+let currentColorIndex = 0;
+
+document.getElementById('changeColorButton').addEventListener('click', function() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    document.body.style.backgroundColor = colors[currentColorIndex];
+});
+
+const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bfbc0'];
+let currentColorIndex = 0;
+
+document.getElementById('bah').addEventListener('click', function() {
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+    document.body.style.backgroundColor = colors[currentColorIndex];
+});
