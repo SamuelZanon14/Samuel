3347cc1b1a839fa8b1571e7b05505246ff4f8b1b
SamuelZanon14 <samuelzanon14@gmail.com>
2024-08-13 17:24:40 -0300

    0644

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
- function clicar(){
-            document.getElementById('name').innerText = 'Jorgeira';
-        }
-
-
-
+function changeFileName() {
+    const newFileName = prompt('Digite o novo nome do arquivo:', 'novo_documento.txt');
+    if (newFileName) {
+        document.getElementById('fileName').innerText = newFileName;
+    }
+}
