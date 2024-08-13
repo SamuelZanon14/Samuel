24810ea95f52079dbc7b6a5c17da25699ecd37ae
SamuelZanon14 <samuelzanon14@gmail.com>
2024-08-13 16:18:29 -0300

    89

--- java.js
+++ java.js
@@ -1,19 +1,8 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    alert('Botão clicado!');
+    // Altera o texto do botão ao ser clicado
+    this.textContent = 'Obrigado!';
+    // Opcional: desabilita o botão após o clique
+    // this.disabled = true;
 });
 
-const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bfbc0'];
-let currentColorIndex = 0;
 
-document.getElementById('changeColorButton').addEventListener('click', function() {
-    currentColorIndex = (currentColorIndex + 1) % colors.length;
-    document.body.style.backgroundColor = colors[currentColorIndex];
-});
-
-const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bfbc0'];
-let currentColorIndex = 0;
-
-document.getElementById('bah').addEventListener('click', function() {
-    currentColorIndex = (currentColorIndex + 1) % colors.length;
-    document.body.style.backgroundColor = colors[currentColorIndex];
-});
