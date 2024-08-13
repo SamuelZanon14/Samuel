35937bbbdb2eb14255d70e88cf231b83c45ca6ea
SamuelZanon14 <samuelzanon14@gmail.com>
2024-08-13 16:43:58 -0300

    049

--- java.js
+++ java.js
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Botão de Nome</title>
-    <style>
-        #name {
-            font-size: 24px;
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
-<body>
-    <div id="name">Nome Original</div>
-    <button onclick="changeName()">Mudar Nome</button>
-
-    <script>
-        function changeName() {
+ function changeName() {
             document.getElementById('name').innerText = 'Nome Novo';
         }
-    </script>
-</body>
-</html>
+
 
 
