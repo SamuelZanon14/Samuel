42394879967051fff3827b855b71e2b6e7be4158
SamuelZanon14 <samuelzanon14@gmail.com>
2024-08-13 16:32:24 -0300

    021

--- java.js
+++ java.js
@@ -1,8 +1,6 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    // Altera o texto do botão ao ser clicado
-    this.textContent = 'Obrigado!';
-    // Opcional: desabilita o botão após o clique
-    // this.disabled = true;
-});
+function clicar(){
+ var a = window.document.getElementById('myButton')   
+ a.innerText = 'Clicou :)'
+}
 
 
